762f929f97d715db49d6b860ac7b17248f87e2c9
unknown <marhaba.munees@gmail.com>
2025-01-31 22:16:36 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+     { path: '', redirectTo:"Home",pathMatch:"full" },
+      { path: 'Home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+];
+
+
+
+
+
